aede37a38229d6b017fa02e81174c23356935294
Sean Chiu <seanphoenixx@gmail.com>
2022-07-11 02:23:14 +0800

    * load tracks

--- ObjectDetection/TrackLoading.swift
+++ ObjectDetection/TrackLoading.swift
@@ -7,3 +7,58 @@
 //
 
 import Foundation
+import AVFoundation
+
+// Ref: https://github.com/rlaguilar/fragmented-MPEG4/blob/d89dd39872c8ea5848bc4bc92c1d6eed7b96eee3/fmp4writer/TrackLoading.swift
+
+struct SourceMedia {
+    let asset: AVAsset
+    let videoTrack: AVAssetTrack
+    let videoTimeScale: CMTimeScale
+}
+
+enum LoadTracksError: Error {
+    case unknown
+    case sourceFileHasNoVideoTrack
+}
+
+func loadTracks(assetURL: URL, completion: @escaping (Result<SourceMedia, Error>) -> Void) {
+    let asset = AVAsset(url: assetURL)
+    let tracksKey = "tracks"
+
+    asset.loadValuesAsynchronously(forKeys: [tracksKey]) {
+        do {
+            var error: NSError? = nil
+            guard asset.statusOfValue(forKey: tracksKey, error: &error) == .loaded else {
+                throw error ?? LoadTracksError.unknown
+            }
+
+            guard let videoTrack = asset.tracks(withMediaType: .video).first else {
+                throw LoadTracksError.sourceFileHasNoVideoTrack
+            }
+
+            // Load and validate the video frame rate.
+            let minFrameDurationKey = "minFrameDuration"
+            let naturalTimeScaleKey = "naturalTimeScale"
+            videoTrack.loadValuesAsynchronously(forKeys: [minFrameDurationKey, naturalTimeScaleKey]) {
+                do {
+                    var error: NSError? = nil
+                    guard videoTrack.statusOfValue(forKey: minFrameDurationKey, error: &error) == .loaded else {
+                        throw error ?? LoadTracksError.unknown
+                    }
+                    guard videoTrack.statusOfValue(forKey: naturalTimeScaleKey, error: &error) == .loaded else {
+                        throw error ?? LoadTracksError.unknown
+                    }
+                    completion(.success(.init(asset: asset,
+                                              videoTrack: videoTrack,
+                                              videoTimeScale: videoTrack.naturalTimeScale)))
+                } catch {
+                    completion(.failure(error))
+                }
+            }
+
+        } catch {
+            completion(.failure(error))
+        }
+    }
+}
